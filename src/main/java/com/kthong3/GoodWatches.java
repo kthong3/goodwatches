2dddda8324cd9a90fb74e29abd91b1b5196b5491
Kadin Van Valin <kadinvanvalin@gmail.com>
2020-08-29 14:47:27 -0400

    init

---
+++ src/main/java/com/kthong3/GoodWatches.java
@@ -0,0 +1,14 @@
+package com.kthong3;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+public class GoodWatches extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res
+    }
+}
